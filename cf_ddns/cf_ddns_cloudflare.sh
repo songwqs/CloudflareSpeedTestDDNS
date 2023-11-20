1ad70e6813613b77027bf5d4ab8610edd8bdc513
songwqs <songwqs@gmail.com>
2023-11-20 18:04:58 +0800

    Update cf_ddns_cloudflare.sh
    修复各种问题

--- cf_ddns/cf_ddns_cloudflare.sh
+++ cf_ddns/cf_ddns_cloudflare.sh
@@ -3,7 +3,7 @@
 #         用于CloudflareST调用，更新hosts和更新cloudflare DNS。
 
 ipv4Regex="((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])";
-
+CLOUDFLAREST_PATH="./cf_ddns/CloudflareST";
 #获取空间id
 zone_id=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones?name=$(echo ${hostname[0]} | cut -d "." -f 2-)" -H "X-Auth-Email: $x_email" -H "X-Auth-Key: $api_key" -H "Content-Type: application/json" | jq -r '.result[0].id' )
 
@@ -85,19 +85,99 @@ fi
 
 # 检查是否配置反代IP
 if [ "$IP_PR_IP" = "1" ] ; then
-  if [[ $(cat ./cf_ddns/.pr_ip_timestamp | jq -r ".pr1_expires") -le $(date -d "$(date "+%Y-%m-%d %H:%M:%S")" +%s) ]]; then
-    curl -sSf -o ./cf_ddns/pr_ip.txt https://cf.vbar.fun/pr_ip.txt
-    echo "{\"pr1_expires\":\"$(($(date -d "$(date "+%Y-%m-%d %H:%M:%S")" +%s) + 86400))\"}" > ./cf_ddns/.pr_ip_timestamp
-    echo "已更新线路1的反向代理列表"
-  fi
+		  if [ -e ./cf_ddns/.pr_ip_timestamp ]; then
+		    # 文件存在
+		    if [[ $(cat ./cf_ddns/.pr_ip_timestamp | jq -r ".pr1_expires") -le $(date -d "$(date "+%Y-%m-%d %H:%M:%S")" +%s) ]]; then
+		        # 文件存在且时间戳小于等于当前时间戳，执行更新操作
+		        curl -sSf -o ./cf_ddns/pr_ip.txt https://cf.vbar.fun/pr_ip.txt
+		        echo "{\"pr1_expires\":\"$(($(date -d "$(date "+%Y-%m-%d %H:%M:%S")" +%s) + 86400))\"}" > ./cf_ddns/.pr_ip_timestamp
+		        echo "已更新线路1的反向代理列表"
+		    fi
+		else
+		    # 文件不存在，执行相应的操作
+		    echo "Error: File ./cf_ddns/.pr_ip_timestamp does not exist. Downloading the file and creating it..."
+		    curl -sSf -o ./cf_ddns/pr_ip.txt https://cf.vbar.fun/pr_ip.txt
+		    echo "{\"pr1_expires\":\"$(($(date -d "$(date "+%Y-%m-%d %H:%M:%S")" +%s) + 86400))\"}" > ./cf_ddns/.pr_ip_timestamp
+		fi
 elif [ "$IP_PR_IP" = "2" ] ; then
-  if [[ $(cat ./cf_ddns/.pr_ip_timestamp | jq -r ".pr2_expires") -le $(date -d "$(date "+%Y-%m-%d %H:%M:%S")" +%s) ]]; then
-    curl -sSf -o ./cf_ddns/pr_ip.txt https://cf.vbar.fun/zip_baipiao_eu_org/pr_ip.txt
-    echo "{\"pr2_expires\":\"$(($(date -d "$(date "+%Y-%m-%d %H:%M:%S")" +%s) + 86400))\"}" > ./cf_ddns/.pr_ip_timestamp
-    echo "已更新线路2的反向代理列表"
-  fi
+	 		if [ -e ./cf_ddns/.pr_ip_timestamp ]; then
+			    # 文件存在
+			    if [[ $(cat ./cf_ddns/.pr_ip_timestamp | jq -r ".pr2_expires") -le $(date -d "$(date "+%Y-%m-%d %H:%M:%S")" +%s) ]]; then
+			        # 文件存在且时间戳小于等于当前时间戳，执行更新操作
+			        curl -sSf -o ./cf_ddns/pr_ip.txt https://cf.vbar.fun/zip_baipiao_eu_org/pr_ip.txt
+			        echo "{\"pr2_expires\":\"$(($(date -d "$(date "+%Y-%m-%d %H:%M:%S")" +%s) + 86400))\"}" > ./cf_ddns/.pr_ip_timestamp
+			        echo "已更新线路2的反向代理列表"
+			    fi
+			else
+			    # 文件不存在，下载文件并执行相应的操作
+			    echo "Error: File ./cf_ddns/.pr_ip_timestamp does not exist. Downloading the file and creating it..."
+			    curl -sSf -o ./cf_ddns/pr_ip.txt https://cf.vbar.fun/zip_baipiao_eu_org/pr_ip.txt
+			    echo "{\"pr2_expires\":\"$(($(date -d "$(date "+%Y-%m-%d %H:%M:%S")" +%s) + 86400))\"}" > ./cf_ddns/.pr_ip_timestamp
+			fi
 fi
-  
+
+
+	# 检查 CloudflareST 是否存在
+	if [ ! -x "$CLOUDFLAREST_PATH" ]; then
+	    echo "CloudflareST not found. Downloading..."
+	
+	    # 获取系统架构
+	    ARCHITECTURE=$(uname -m)
+	    
+	    # 获取最新版本号
+	    LATEST_VERSION=$(curl -s "https://api.github.com/repos/XIU2/CloudflareSpeedTest/releases/latest" | jq -r .tag_name)
+	    echo "最新版本$LATEST_VERSION"
+	    if [ "$LATEST_VERSION" == "null" ]; then
+	        echo "Failed to fetch the latest version from GitHub API."
+	        exit 1
+	    fi
+
+	     # 判断架构并设置对应的下载链接
+    case $ARCHITECTURE in
+        x86_64)
+            DOWNLOAD_URL="https://git.songw.top/https://github.com/XIU2/CloudflareSpeedTest/releases/download/$LATEST_VERSION/CloudflareST_linux_amd64.tar.gz"
+            ;;
+        armv7l)
+            DOWNLOAD_URL="https://git.songw.top/https://github.com/XIU2/CloudflareSpeedTest/releases/download/$LATEST_VERSION/CloudflareST_linux_armv7.tar.gz"
+            ;;
+        aarch64)
+            DOWNLOAD_URL="https://git.songw.top/https://github.com/XIU2/CloudflareSpeedTest/releases/download/$LATEST_VERSION/CloudflareST_linux_arm64.tar.gz"
+            ;;
+        *)
+            echo "没找到这个架构: $ARCHITECTURE 可以去项目地址看看 https://github.com/XIU2/CloudflareSpeedTest/releases/tag/$LATEST_VERSION"
+            exit 1
+            ;;
+    esac
+	    TMP_ARCHIVE="/tmp/CloudflareST.tar.gz"
+	    # 下载最新版本 CloudflareST
+	    curl -L -o "$TMP_ARCHIVE" "$DOWNLOAD_URL"
+	
+	    # 解压缩文件
+	    tar -xzvf "$TMP_ARCHIVE" -C ./cf_ddns/
+	    
+	    # 删除临时压缩文件
+	    rm "$TMP_ARCHIVE"
+	    # 设置执行权限
+    	  chmod +x "$CLOUDFLAREST_PATH"
+	
+	    echo "CloudflareST downloaded and installed successfully."
+	fi
+	# 检查 result.csv 文件是否存在，如果不存在则创建
+	if [ ! -e ./cf_ddns/result.csv ]; then
+	    touch ./cf_ddns/result.csv
+	fi
+
+
+ if [ ! -e "./cf_ddns/pr_ip.txt" ]; then
+    if [ -e "./cf_ddns/ip.txt" ]; then
+        mv "./cf_ddns/ip.txt" "./cf_ddns/pr_ip.txt"
+    else
+    	   curl -sSf -o ./cf_ddns/pr_ip.txt https://cf.vbar.fun/pr_ip.txt
+        echo "默认下载ip-scanner/cloudflare仓库"
+    fi
+else
+    echo "pr_ip.txt already exists."
+fi
 if [ "$IP_PR_IP" -ne "0" ] ; then
   $CloudflareST $CFST_URL_R -t $CFST_T -n $CFST_N -dn $CFST_DN -tl $CFST_TL -dt $CFST_DT -tp $CFST_TP -sl $CFST_SL -p $CFST_P -tlr $CFST_TLR $CFST_STM -f ./cf_ddns/pr_ip.txt -o ./cf_ddns/result.csv
 elif [ "$IP_ADDR" = "ipv6" ] ; then
@@ -130,7 +210,7 @@ while [[ ${x} -lt $num ]]; do
   # 获取优选后的ip地址
   ipAddr=$(sed -n "$((x + 2)),1p" ./cf_ddns/result.csv | awk -F, '{print $1}');
   ipSpeed=$(sed -n "$((x + 2)),1p" ./cf_ddns/result.csv | awk -F, '{print $6}');
-  if [ $ipSpeed = "0.00" ]; then
+  if [ "$ipSpeed" == "0.00" ]; then
     echo "第$((x + 1))个---$ipAddr测速为0，跳过更新DNS，检查配置是否能正常测速！";
   else
     if [ "$IP_TO_HOSTS" = 1 ]; then
@@ -193,4 +273,3 @@ if [ "$IP_TO_HOSTS" = 1 ]; then
     rm ./cf_ddns/hosts_new
   fi
 fi
-
